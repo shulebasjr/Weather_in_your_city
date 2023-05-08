2fb86c4751ff1faf251ce3803463daa609ec8d5a
shulebasjr <shulenin.m@mail.ru>
2023-05-08 14:06:12 +0200

    added readme and main.py

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Weather_in_your_city
+# weather in your city
+programm that can tell you about the weather in your city
+
+## How to build project
+No need to build as it´s on python
+
+## How to run project
+Work is in progress now
+but you can do it with commands:
+```bash
+cd src
+python main.py
+```
---
+++ src/modules/main.py
@@ -0,0 +1,18 @@
+from pyowm import OWM              
+
+owm = OWM('626fc1698f8a355b4e8ed609c24f22d5')
+mgr = owm.weather_manager()
+
+place = input("input your city?:")
+
+observation = mgr.weather_at_place(place)
+w = observation.weather
+
+temp = w.temperature('celsius')['temp']
+
+if temp < 10:
+    print('It is very cold outside, you would better put something warm on you')
+elif temp < 20:
+    print('It is pretty cold outside, put something on you')
+else:
+    print('The temperature is okay, you can put whatever you want on you')
